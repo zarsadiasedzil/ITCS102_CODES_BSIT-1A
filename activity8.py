ecc497259cb277d0a3581bb085c939f14d9d98b3
Zarsadias, Edzil E. <edzilzarsadias01@gmail.com>
2026-09-03 11:16:39 +0800

    Update activity8.py

--- activity8.py
+++ activity8.py
@@ -1,1 +1,13 @@
-hb = ("What is your hobbies?")
+hobbies = " " #blank string
+
+hb = input("What is your hobbies?")
+hobbies += hb + ", "
+
+hb = input("What else?")
+hobbies += hb + ", "
+
+hb = input("What else?")
+hobbies += hb 
+
+hb = input("What else?")
+print("My hobbies are", hobbies)
