c92a0d1482b6cbecabbb9984dd9651b053f763eb
Zarsadias, Edzil E. <edzilzarsadias01@gmail.com>
2026-09-03 11:28:01 +0800

    Update code_challenge2.py

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,16 +1,9 @@
 #code challenge 2
+# money = 4567
+# 1000, 500, 200, 100, 50, 20, 10, 5, 1
+# What operator/ symbol did you use to solve the problem?
+# floor division, % modulus
 
 money = 4567
 
-a =1000
-b =500
-c =200
-d =100
-e =50
-f =20
-g =10
-h =5 
-i =1
-
-print("The amount of money",a,"is",money//a)
-print("The amount of money",b,"is",money-a)
+thousand = money // 1000
