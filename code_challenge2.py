90961deb40c4237e3bca10896862d6e7529d7ea5
Zarsadias, Edzil E. <edzilzarsadias01@gmail.com>
2026-09-03 11:43:35 +0800

    Update code_challenge2.py

--- code_challenge2.py
+++ code_challenge2.py
@@ -7,24 +7,24 @@
 money = 4567
 
 #computation here
-thousand = money // 1000
-five_hund = (money % 1000) // 500
-two_hund = (money % 1000 % 500) // 200
-one_hund = (money % 1000 % 500 % 200) // 100
-fifty = (money % 1000 % 500 % 200 % 100) // 50
-twenty = (money % 1000 % 500 % 200 % 100 % 50) // 20
-ten = (money % 1000 % 500 % 200 % 100 % 50 % 20) // 10
-five = (money % 1000 % 500 % 200 % 100 % 50 % 20 % 10) // 5
-one = (money % 1000 % 500 % 200 % 100 % 50 % 20 % 10 % 5) // 1
+a = money // 1000
+b = (money % 1000) // 500
+c = (money % 1000 % 500) // 200
+d = (money % 1000 % 500 % 200) // 100
+e = (money % 1000 % 500 % 200 % 100) // 50
+f = (money % 1000 % 500 % 200 % 100 % 50) // 20
+g = (money % 1000 % 500 % 200 % 100 % 50 % 20) // 10
+h = (money % 1000 % 500 % 200 % 100 % 50 % 20 % 10) // 5
+i = (money % 1000 % 500 % 200 % 100 % 50 % 20 % 10 % 5) // 1
 
 #prints here
-print("1000 = ",thousand)
-print(" 500 = ",five_hund)
-print(" 200 = ",two_hund)
-print(" 100 = ",one_hund)
-print("  50 = ",fifty)
-print("  20 = ",twenty)
-print("  10 = ",ten)
-print("   5 = ",five)
-print("   1 = ",one)
+print("1000 = ",a)
+print(" 500 = ",b)
+print(" 200 = ",c)
+print(" 100 = ",d)
+print("  50 = ",e)
+print("  20 = ",f)
+print("  10 = ",g)
+print("   5 = ",h)
+print("   1 = ",i)
 print("The total money deposited are",money)
