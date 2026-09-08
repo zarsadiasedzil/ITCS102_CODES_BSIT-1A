dc9337fec6558e4cf4dadb1f39e2d20311f976f2
Zarsadias, Edzil E. <edzilzarsadias01@gmail.com>
2026-09-08 10:46:15 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,11 @@
+#logical operator demo
+x = 11
+y= 21
+username= "user1"
+Username = "exploration"
+
+print (x>=y)
+print (x>=y and username=="user1")
+print (x>=y or username== "user1")
+print (not(x>=y or username== "user1"))
+print (x>=y or username== "user1" and Username == "exploration")
---
+++ activity9.py
@@ -0,0 +1,37 @@
+#relational operator demo
+x = 11
+y = 21
+X = 5
+Y = 5
+user1 = "zarsadias"
+user2 = "Zarsadias"
+
+#>
+print(x>y)
+print(X>Y)
+print(user1>user2)
+
+#<
+print(x<y)
+print(X<Y)
+print(user1<user2)
+
+#>=
+print(x>=y)
+print(X>=Y)
+print(user1>=user2)
+
+#<=
+print(x<=y)
+print(X<=Y)
+print(user1<=user2)
+
+#==
+print(x==y)
+print(X==Y)
+print(user1==user2)
+
+#!=
+print(x!=y)
+print(X!=Y)
+print(user1!=user2)
