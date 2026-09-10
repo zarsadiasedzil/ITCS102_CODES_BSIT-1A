caf3146e84a4bc8c898954ab5dd3911066bab1c3
Zarsadias, Edzil E. <edzilzarsadias01@gmail.com>
2026-09-10 10:40:14 +0800

    Update activity13.py

--- activity13.py
+++ activity13.py
@@ -1,30 +1,30 @@
 #Write a python program that accepts an integer number as age
 # and determinves the age group label base of that age input
 
-name = input("Input Name -->")
-age= int(input("Input Age -->"))
+name = input("Input NAME -->")
+age= int(input("Input AGE -->"))
 
-print("Hi",name, "That age is considered as")
+print("Hi,",name, "that age is considered as")
 if age>=1 and age<=5:
-	print("infant")
+	print("Infant")
 
 elif age>=6 and age<=12:
-	print("kid")
+	print("Kid")
 
 elif age>=13 and age<=19:
-	print("teenager")
+	print("Teenager")
 
 elif age>=20 and age<=29:
-	print("early adult")
+	print("Early adult")
 
 elif age>=30 and age<=48:
-	print("adult")
+	print("Adult")
 
 elif age>=49 and age<=59:
-	print("advanced adult")
+	print("Advanced adult")
 
 elif age>=60 and age<=150:
-	print("senior")
+	print("Senior")
 
 else:
-	print("invalid")
+	print("Invalid")
