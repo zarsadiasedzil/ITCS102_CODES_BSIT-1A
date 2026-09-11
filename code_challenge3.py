364da96d6740d8b766e0b87d63f8c76d34ba35ff
Zarsadias, Edzil E. <edzilzarsadias01@gmail.com>
2026-09-11 14:36:08 +0800

    Refactor global freight calculator for clarity
    Edzil's trip
    Sorry na wala akong magawa sa buhay ko.

--- code_challenge3.py
+++ code_challenge3.py
@@ -4,11 +4,11 @@
 #Simple Variables to follow
 name = input("Input Name: ")
 item = input("Type of Item: ")
-is_fragile = input("It is Fragile?(Yes/No): ").strip().lower() == "yes"
+is_fragile = input("It is Fragile?(Yes/No): ") == "yes"
 weight = float(input("Item weight in KG: "))
 distance = float(input("Distance in KM: "))
-is_express = input("Item express(True/False): ").strip().lower() == "true"
-is_international = input("International(True/False): ").strip().lower() == "true"
+is_express = input("Item express(True/False): ") == "true"
+is_international = input("International(True/False): ") == "true"
 
 # Calculation of Base Cost
 base_cost = (weight * 2.50) + (distance * 0.15)
@@ -28,13 +28,13 @@ else:
 
 # Expected Output or Outcome when Printed
 print("\n---Resibo---")
-print("Tumanggap:", name)
-print("Pangalan ng produkto",item)
-print("Fragile?:", is_fragile)
-print("Timbang:",weight,"KG" )
-print("Distansya o Ambulansya?:",distance,"KM")
-print("Express:", is_express)
-print("International:", is_international)
-print("Kabuuang Halaga:", total)
-print("\n\nO")
+print("Tumanggap ng Produkto:", name)
+print("Pangalan ng produkto: ",item)
+print("Marupok kaba?: ", is_fragile)
+print("Timbang: ",weight,"KG" )
+print("Didistansya o Ambulansya?: ",distance,"KM")
+print("Express: ", is_express)
+print("Pandaigdigan: ", is_international)
+print("Kabuuang Halagang Dapat Bayaran: ", total)
+print("\n\nOr")
 print("\n\nThe order has been dilevered and received by Mr/Mrs.", name, ", product name", item,", weight of product", weight,",and the total cost is", total)
