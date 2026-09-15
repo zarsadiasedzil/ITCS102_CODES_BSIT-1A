967a7d94d65b2225bb6e59817d62097676afad6f
Zarsadias, Edzil E. <edzilzarsadias01@gmail.com>
2026-09-15 21:32:25 +0800

    Update activity14.py

--- activity14.py
+++ activity14.py
@@ -4,24 +4,25 @@
 # annual_income (float)
 # has_collateral (boolean)
 
-age = int(input("Your age: "))
-is_employed = bool(input("Are you employed?(True or False): ")) == "true"
+age = int(input("Enter your age: "))
+is_employed = bool(input("Are you employed? (True or False): "))
 credit_score = float(input("What's your credit score: "))
-annoul_income = float(input("What's your annoul income: "))
-has_collateral = bool(input("Do you have collateral?(True or False): ")) == "true"
+annual_income = float(input("What's your annual income: "))
+has_collateral = bool(input("Have collateral? (True or False): "))
 
-# Baseline eligibility
-baseline = 0
+# base interest
+interest_rate = 0.0
 
-# Financial Evaluation
-if age >= 21 and is_employed == True
+# calculations
+if age >= 21 and is_employed == True :
+  if age >= 21 and is_employed:
     print("Accepted: You're able to loan.")
     if credit_score >= 750:
         if annual_income >= 100000:
             interest_rate = 4.5
         else:
             interest_rate = 5.0
-        print("Approvedand has ", interest_rate, "% interest rate")
+        print("Approved and has:", interest_rate, "% interest rate")
     elif credit_score >= 600:
         if has_collateral:
             interest_rate = 7.0
@@ -29,8 +30,8 @@ if age >= 21 and is_employed == True
             interest_rate = 9.5
         else:
             interest_rate = 8.0
-        print("Approved and has ", interest_rate, "% interest rate")
+        print("Approved and has:", interest_rate, "% interest rate")
     else:
-        print("Rejected: Low credit score for loaning.")
+        print("Rejected: Does not meet the required annoul income")
 else:
-    print("Rejected: Requirements not meet.")
+    print("Rejected: Requirements not meet to loan.")
